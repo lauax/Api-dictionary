947f7a365e542cb03ee8b9e29f1694e28b1ee037
lauax <lucas12alfredsson@gmail.com>
2023-11-29 13:53:50 +0100

    påbörjat

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <form>
+      <input type="submit" value="Submit" name="searchUser" />
+    </form>
+  );
+}
+
+export default App;
